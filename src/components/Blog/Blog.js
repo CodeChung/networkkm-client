fc134ba4a1d05bec6049a6ed497d2ad0e030da31
Harry Chung <hc9825@gmail.com>
2019-11-24 12:52:47 -0800

    added readme

--- src/components/Blog/Blog.js
+++ src/components/Blog/Blog.js
@@ -1,10 +1,15 @@
 import React from 'react'
+import { Button } from '../Utils/Utils'
+import { TiFeather } from 'react-icons/ti'
+import './Blog.css'
+
 
 class Blog extends React.Component {
     render() {
         return (
             <div className='blog'>
                 Kenny Bloggins
+                <Button className='new-blog-post'><TiFeather /> New Post</Button>
             </div>
         )
     }
